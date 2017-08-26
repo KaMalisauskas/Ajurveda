14e09a32bba0d75c3bba3ed6133f4c59ea49575e
Karoluks <karolis.malisauskas@gmail.com>
2017-08-26 20:56:30 +0300

    Starting to imploment Js

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Karolis");
+// var x = document.getElementById("fade").style.background;
+// console.log(x);
+
+var img = document.getElementById('fade'),
+style = img.currentStyle || window.getComputedStyle(img, false);
+bi = style.backgroundImage.slice(4, -1);
+console.log(bi);
+
+function classToggle() {
+    var picture = this.classList.toggle('insertPicture');
+    this.classList.toggle('insertPicture2');
+}
+document.querySelector('#fade').addEventListener('click', classToggle);
