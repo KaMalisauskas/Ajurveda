8ea39a312e5be8a6cd01ca08ff9d28c79e84ecbb
Karoluks <karolis.malisauskas@gmail.com>
2017-08-28 14:36:43 +0300

    Update

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,10 @@
 // var x = document.getElementById("fade").style.background;
 // console.log(x);
 
-var img = document.getElementById('fade'),
-style = img.currentStyle || window.getComputedStyle(img, false);
-bi = style.backgroundImage.slice(4, -1);
-console.log(bi);
+// var img = document.getElementById('fade'),
+// style = img.currentStyle || window.getComputedStyle(img, false);
+// bi = style.backgroundImage.slice(4, -1);
+// console.log(bi);
 
 function classToggle() {
     var picture = this.classList.toggle('insertPicture');
@@ -13,8 +13,8 @@ function classToggle() {
 document.querySelector('#fade').addEventListener('click', classToggle);
 
 
-function imgMax() {
-  var picture1 = this.classList.toggle("image");
-  this.classList.toggle("img_onClick");
-}
-document.querySelector("#resize").addEventListener("click", imgMax);
+// function imgMax() {
+//   var picture1 = this.classList.toggle("image");
+//   this.classList.toggle("img_onClick");
+// }
+// document.querySelector("#resize").addEventListener("click", imgMax);
